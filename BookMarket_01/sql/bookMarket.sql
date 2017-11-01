07ec822543b229a50fb205e9b35cd41a3d743993
student <student@M110111>
2017-11-01 15:05:45 +0900

    sql-update(2017-11-01 15:04pm) book table: title 글자수 확장 member, book: data(insert)

--- BookMarket_01/sql/bookMarket.sql
+++ BookMarket_01/sql/bookMarket.sql
@@ -2,12 +2,8 @@ SET CHARACTER SET 'UTF-8';
 SET CHARACTER SET 'EUCKR';
 
 DROP TABLE IF EXISTS member, book, sold, deal, keepBook, money;
-DROP TABLE IF EXISTS book;
-DROP TABLE IF EXISTS sold;
-DROP TABLE IF EXISTS deal;
-DROP TABLE IF EXISTS keepBook;
-DROP TABLE IF EXISTS money;
 
+DROP TABLE IF EXISTS member;
 CREATE TABLE member(
 mem_id INT NOT NULL AUTO_INCREMENT,
 email varchar(18) default null,
@@ -17,11 +13,12 @@ join_date DATE,
 PRIMARY KEY (mem_id)
 );
 
+DROP TABLE IF EXISTS book;
 CREATE TABLE book(
 book_id INT NOT NULL AUTO_INCREMENT,
 isbn VARCHAR(20) NOT NULL,
 author VARCHAR(20),
-title VARCHAR(20) NOT NULL,
+title VARCHAR(100) NOT NULL,
 publisher VARCHAR(20),
 published_date DATE,
 genre INT,
@@ -37,6 +34,7 @@ comment_img VARCHAR(1000),
 PRIMARY KEY (book_id)
 );
 
+DROP TABLE IF EXISTS sold;
 CREATE TABLE sold(
 order_id INT AUTO_INCREMENT,
 seller_id INT,
@@ -47,6 +45,7 @@ buyer_id INT,
 PRIMARY KEY (order_id)
 );
 
+DROP TABLE IF EXISTS deal;
 CREATE TABLE deal(
 deal_idx INT AUTO_INCREMENT, 
 participant_id INT,
@@ -56,6 +55,7 @@ sold_state INT DEFAULT 0,
 PRIMARY KEY (deal_idx)
 );
 
+DROP TABLE IF EXISTS keepBook;
 CREATE TABLE keepBook(
 keepBook_id	int AUTO_INCREMENT,
 book_id	int,
@@ -64,6 +64,7 @@ keep_date DATE,
 PRIMARY KEY (keepBook_id)
 );
 
+DROP TABLE IF EXISTS money;
 CREATE TABLE money(
 money_idx INT AUTO_INCREMENT,
 mem_id	int,
@@ -71,5 +72,30 @@ money	int,
 money_update_day DATE,
 PRIMARY KEY (money_idx)
 );
+
+INSERT INTO member VALUES ('0','a1@gmail.com','111','a1A1','1980-12-17');
+INSERT INTO member VALUES ('0','a2@gmail.com','222','a2A2','1980-12-17');
+INSERT INTO member VALUES ('0','a3@gmail.com','333','a3A3','1980-12-17');
+INSERT INTO member VALUES ('0','a4@gmail.com','444','a4A4','1980-12-17');
+INSERT INTO member VALUES ('0','a5@gmail.com','555','a5A5','1980-12-17');
+INSERT INTO member VALUES ('0','a6@gmail.com','666','a6A6','1980-12-17');
+INSERT INTO member VALUES ('0','a7@gmail.com','777','a7A7','1980-12-17');
+INSERT INTO member VALUES ('0','a8@gmail.com','888','a8A8','1980-12-17');
+
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9791187165286','Carol Dweck', 'Mind Set', 5, 9900);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9791187165286','Carol Dweck', 'Mind Set', 4, 5900);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9791187165286','Carol Dweck', 'Mind Set', 1, 3000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788959894826','커넥팅 랩', '모바일 트렌드 2018', 4, 8000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788959894826','커넥팅 랩', '모바일 트렌드 2018', 2, 5000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788925561776','주홍식', '스타벅스, 공간을 팝니다 하워드 슐츠가 감탄한 스타벅스커피 코리아 1조 매출의 비밀 ', 5, 10000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788950959227','데릭 톰슨', '히트 메이커스 세상을 사로잡은 히트작은 어떻게 만들어졌는가', 4, 14000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788959894178','브렛 킹', '증강현실 현실 위의 현실, 슈퍼 리얼리티의 세계가 열린다,Life in the Smart Lane', 4, 18000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9791155323076','권광현, 박영훈', '디지털 노마드 직장 없이 자유롭게 돈 버는 사람들', 5, 10000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788984373327','조엘 디케르', '볼티모어의 서', 5, 10000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788984371231','더글라스 케네디', '리빙 더 월드', 3, 5000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9788984371224','안데르스 데 라 모테', '게임', 4, 7700);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('9780062464316','HARARI, YUVAL NOAH', 'Homo Deus A History of Tomorrow', 5, 30000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('0070856133','Rudin', 'Principles of Mathematical Analysis, 3/E International Series in Pure & Applied Mathematics', 5, 30000);
+INSERT INTO book (isbn, author, title, book_condition, price) VALUES ('0684853949','Sacks, Oliver W.', 'The Man Who Mistook His Wife for a Hat And Other Clinical Tales', 3, 3000);
 
 
