5f814b8437d0dbcbce81669a6d23b77983bf337b
student <student@M110112>
2017-11-01 16:01:07 +0900

    dao

--- BookMarket_01/src/dao/IKeepBookDao.java
+++ BookMarket_01/src/dao/IKeepBookDao.java
@@ -7,6 +7,6 @@ import dto.KeepBook;
 
 public interface IKeepBookDao {
 	public int insertKeepBook(KeepBook keepBook); // 찜내역 추가하는 메소드
-	public List<KeepBook> selectKeepBookList(int mem_id); // 멤버 번호로 찜 내역 얻어오는메소드 
+	public List<KeepBook> selectKeepBookList(int mem_id); // 멤버 번호로 찜 내역 얻어오는메소드 날짜순으로 order
 
 }
--- BookMarket_01/src/dao/ISoldDao.java
+++ BookMarket_01/src/dao/ISoldDao.java
@@ -6,7 +6,7 @@ import dto.Sold;
  
 public interface ISoldDao {
 	public int insertSold(Sold sold); // 거래완료내역 추가하는 메소드
-	public List<Sold> selectSoldListBySeller(int seller_id); // 판매자가 불러오는 거래완료내역 
+	public List<Sold> selectSoldListBySeller(int seller_id); // 판매자가 불러오는 거래완료내역  
 	public  List<Sold> selectSoldListByBuyer(int buyer_id); //구매자가 불러오는 거래완료내역 
-
+   // 거래내역 날짜순으로
 }
