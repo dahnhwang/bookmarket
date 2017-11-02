f975370ba9ba35b23c1fcb099fdde4338c5bf1fc
student <student@M110111>
2017-11-02 15:50:38 +0900

    Login.jsp, LoginAction.java// 1차 완성

--- BookMarket_01/src/controller/action/LoginAction.java
+++ BookMarket_01/src/controller/action/LoginAction.java
@@ -8,12 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.BoardDAO;
+import dao.IMemberDao;
+import dao.MemberDao;
+import dto.BoardVO;
+import dto.Member;
+
 public class LoginAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		 
+//		new MainAction().execute(request, response);
+		HttpSession session = request.getSession();
+		String url = null;
+		String email = request.getParameter("email");
+		String pwd = request.getParameter("password");
+		IMemberDao mDao = MemberDao.getInstance();
+		int mem_id = mDao.memberCheck(email, pwd);
+		Member member = null;
+		if(mem_id>0) { // 성공 
+			member = mDao.getMember(mem_id);
+			session.setAttribute("loginUser", member);
+			url="main.jsp";
+			
+		}else if (mem_id == 0) {
+			request.setAttribute("message", "비밀번호가 틀렸습니다");
+			url="/member/login.jsp";
+		} else if(mem_id == -1){
+			request.setAttribute("message", "회원이 아닙니다.");
+			url="/member/login.jsp";
+		}
+		RequestDispatcher disp = request.getRequestDispatcher(url);
+		disp.forward(request, response);
 	}
 
 }
