a7240ee5be006a89dd83f213c1613125928db609
student <student@M110112>
2017-11-03 17:51:22 +0900

    bookList 페이지 command치고 들어가셔야돼요

--- BookMarket_01/src/controller/action/BookListAction.java
+++ BookMarket_01/src/controller/action/BookListAction.java
@@ -10,8 +10,9 @@ public class BookListAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String url = "/book/bookList.jsp";
 		
+		request.getRequestDispatcher(url).forward(request, response);		
 	}
 
 }
