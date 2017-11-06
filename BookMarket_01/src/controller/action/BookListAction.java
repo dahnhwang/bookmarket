b0b6b0f11fc89faf779026e728784f5eb5462994
student <student@M110112>
2017-11-06 17:09:01 +0900

    booklist 추가 db 불러오기

--- BookMarket_01/src/controller/action/BookListAction.java
+++ BookMarket_01/src/controller/action/BookListAction.java
@@ -2,17 +2,24 @@ package controller.action;
 
 import java.io.IOException;
 
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.BookDao;
+import dto.Book;
 
 public class BookListAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "/book/bookList.jsp";
-		
+		BookDao bdo = BookDao.getInstance();
+		List<Book> bookList =  bdo.selectAllBookList();
+		request.setAttribute("bookList", bookList);
 		request.getRequestDispatcher(url).forward(request, response);		
 	}
 
-}
+} 
