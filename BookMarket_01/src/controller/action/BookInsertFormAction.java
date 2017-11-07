fdef28273920c8172de39a5cdb9606d96ae3ae6c
dahyun hwang <amsterdahn@gmail.com>
2017-11-07 15:00:15 +0900

    api 구동테스트 완료
    Signed-off-by: dahyun hwang <amsterdahn@gmail.com>

--- BookMarket_01/src/controller/action/BookInsertFormAction.java
+++ BookMarket_01/src/controller/action/BookInsertFormAction.java
@@ -1,11 +1,16 @@
 package controller.action;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dto.Book;
 import util.NaverApi;
 
 public class BookInsertFormAction implements Action {
@@ -15,10 +20,24 @@ public class BookInsertFormAction implements Action {
 		String url="book/insertBook.jsp?isbn=";
 		
 		String isbn = request.getParameter("isbn");
-		isbn = "9788937473135";
+//		isbn = "9788937473135";
 		NaverApi api = new NaverApi();
-		api.getJson(isbn);
-		
+		List<HashMap<String, Object>> list = api.getJson(isbn);
+		Iterator iter = list.iterator();
+		Book book = null;
+		while(iter.hasNext()) {
+			Map<String, Object> map = (HashMap<String, Object>)iter.next();
+			book = new Book();
+			book.setTitle((String)map.get("title"));
+			book.setIsbn((String)map.get("isbn"));
+			book.setImage((String)map.get("image"));
+			book.setAuthor((String)map.get("author"));
+			book.setPublisher((String)map.get("publisher"));
+			book.setPublished_date((String)map.get("pubdate"));
+			book.setDescription((String)map.get("description"));
+		}
+		System.out.println(book.toString());
+		request.setAttribute("book", book);
 		request.getRequestDispatcher(url+isbn).forward(request, response);
 	}
 
--- BookMarket_01/src/util/NaverApi.java
+++ BookMarket_01/src/util/NaverApi.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -15,10 +17,10 @@ public class NaverApi {
 	private static String clientId = "gI6kVX6iQIwugQqKWFRz";
 	private static String clientSecret = "39waYl4FGS";
 
-	public Map<String, Object> getJson(String isbn) {
-		Map<String, Object> item = null;
+	public List<HashMap<String, Object>> getJson(String isbn) {
 
-		// 네이버API에 isbn에 대한 데이터 요청을 보내고 응답을 파싱해서 결과를 리턴해주는 메소드
+		List<HashMap<String, Object>> items = null;
+
 		try {
 			String apiURL = "https://openapi.naver.com/v1/search/book?query=" + isbn; // json 결과
 			URL url = new URL(apiURL);
@@ -39,25 +41,18 @@ public class NaverApi {
 				response.append(inputLine);
 			}
 			br.close();
-			// json에서 파싱하기!
-			
-			item = new HashMap<String, Object>();
-			ObjectMapper mapper = new ObjectMapper(); // Json Parser
-			mapper.readValue(response.toString(), new TypeReference<HashMap<String, String>>() {
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			ObjectMapper mapper = new ObjectMapper();
+			map = mapper.readValue(response.toString(), new TypeReference<HashMap<String, Object>>() {
 			});
-			item = (Map<String, Object>)item.get("items");
-			System.out.println(item);
-//			
-//			System.out.println(response.toString());
-			// response <- 요놈아가 내가 받을 api데이터임
-			// 여기서 map으로 얻어진 items는 어레이리스트로 자동으로 변환되어서 옴
-			// 따라서 arrayList에 넣어줘야 함~~
+			items = (ArrayList<HashMap<String, Object>>) map.get("items");
 
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
-		return item;
+		return items;
 
 	}
 
