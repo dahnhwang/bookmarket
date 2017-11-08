ed3589a06ce47b8adb30295afcd242cbec117e66
dahyun hwang <amsterdahn@gmail.com>
2017-11-08 14:34:40 +0900

    KeepBookDao & SoldDao 리뷰완료 (테스트는 x)
    Signed-off-by: dahyun hwang <amsterdahn@gmail.com>

--- BookMarket_01/src/dao/KeepBookDao.java
+++ BookMarket_01/src/dao/KeepBookDao.java
@@ -1,21 +1,105 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dto.KeepBook;
 
-public class KeepBookDao implements IKeepBookDao{
+public class KeepBookDao implements IKeepBookDao {
+
+	private Connection conn;
+
+	private static KeepBookDao instance;
+
+	public static KeepBookDao getInstance() {
+		if (instance == null)
+			instance = new KeepBookDao();
+		return instance;
+	}
+
+	private KeepBookDao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookmarket_db", "root", "mysql");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public int insertKeepBook(KeepBook keepBook) {
 		// TODO Auto-generated method stub
-		return 0;
+		String sql = "INSERT INTO keepbook VALUES(0,?,?,?)";
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, keepBook.getBook_id());
+			pstmt.setInt(2, keepBook.getMem_id());
+			pstmt.setDate(3, new java.sql.Date(keepBook.getKeep_date().getTime()));
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 
 	@Override
 	public List<KeepBook> selectKeepBookList(int mem_id) {
 		// TODO Auto-generated method stub
-		return null;
+		List<KeepBook> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM keepbook WHERE mem_id=?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mem_id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				KeepBook kb = new KeepBook();
+
+				kb.setKeepBook_id(rs.getInt("keepBook_id"));
+				kb.setBook_id(rs.getInt("book_id"));
+				kb.setMem_id(rs.getInt("mem_id"));
+				kb.setKeep_date(rs.getDate("keep_date"));
+
+				list.add(kb);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return list;
+
 	}
 
 }
--- BookMarket_01/src/dao/SoldDao.java
+++ BookMarket_01/src/dao/SoldDao.java
@@ -1,27 +1,152 @@
 package dao;
-
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
+ 
 import dto.Sold;
-
+ 
 public class SoldDao implements ISoldDao{
-
+	private Connection conn;
+ 
+	private static SoldDao instance;
+ 
+	public static SoldDao getInstance() {
+		if (instance == null)
+			instance = new SoldDao();
+		return instance;
+	}
+ 
+	private SoldDao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookmarket_db", "root", "mysql");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+ 
 	@Override
 	public int insertSold(Sold sold) {
 		// TODO Auto-generated method stub
-		return 0;
+		String sql = "INSERT INTO sold VALUES(0,?,?,?,?,?)";
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, sold.getSeller_id());
+			pstmt.setInt(2, sold.getBook_id());
+			pstmt.setDate(3, new java.sql.Date(sold.getSold_date().getTime()));
+			// 선생님께서 작성해주신 코드 (Date객체를 sql안에 넣을 때 사용)  
+			pstmt.setInt(4, sold.getSold_price());
+			pstmt.setInt(5	, sold.getBuyer_id());
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
-
+	
+ 
 	@Override
 	public List<Sold> selectSoldListBySeller(int seller_id) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Sold> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM sold WHERE seller_id=?";
+ 
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, seller_id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Sold sd = new Sold();
+				
+				sd.setOrder_id(rs.getInt("order_id"));
+				sd.setSeller_id(rs.getInt("seller_id"));
+				sd.setBook_id(rs.getInt("book_id"));
+				sd.setSold_date(rs.getDate("sold_date"));
+				sd.setSold_price(rs.getInt("sold_price"));
+				sd.setBuyer_id(rs.getInt("buyer_id"));
+				
+				list.add(sd);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+ 
+		return list;
 	}
-
+ 
 	@Override
 	public List<Sold> selectSoldListByBuyer(int buyer_id) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Sold> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM sold WHERE buyer_id=?";
+ 
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, buyer_id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Sold sd = new Sold();
+				
+				sd.setOrder_id(rs.getInt("order_id"));
+				sd.setSeller_id(rs.getInt("seller_id"));
+				sd.setBook_id(rs.getInt("book_id"));
+				sd.setSold_date(rs.getDate("sold_date"));
+				sd.setSold_price(rs.getInt("sold_price"));
+				sd.setBuyer_id(rs.getInt("buyer_id"));
+				
+				list.add(sd);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+ 
+		return list;
 	}
-
+ 
 }
