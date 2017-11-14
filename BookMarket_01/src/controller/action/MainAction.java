233512534ed2b1962fd6f18bc88a6209dc0e0a0e
student <student@M110111>
2017-11-14 17:00:09 +0900

    완성!!!!

--- BookMarket_01/src/controller/action/MainAction.java
+++ BookMarket_01/src/controller/action/MainAction.java
@@ -5,12 +5,22 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.Member;
 
 public class MainAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	String url = "main.jsp";
+	HttpSession session = request.getSession();
+	Member loginUser = null;
+	if(session.getAttribute("loginUser")!=null) {
+		loginUser = (Member) session.getAttribute("loginUser");
+		System.out.println(loginUser);
+	}
+	session.setAttribute("loginUser", loginUser);
 	
 	request.getRequestDispatcher(url).forward(request, response);
 	
