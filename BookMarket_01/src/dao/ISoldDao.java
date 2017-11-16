20039891c5e2a956901df0635ca6795b22eec24c
student <student@M110111>
2017-11-16 16:14:34 +0900

    ISoldDao도

--- BookMarket_01/src/dao/ISoldDao.java
+++ BookMarket_01/src/dao/ISoldDao.java
@@ -9,4 +9,5 @@ public interface ISoldDao {
 	public List<Sold> selectSoldListBySeller(int seller_id); // 판매자가 불러오는 거래완료내역  
 	public  List<Sold> selectSoldListByBuyer(int buyer_id); //구매자가 불러오는 거래완료내역 
    // 거래내역 날짜순으로
+	public List<Sold> selectSoldListByLoginUser(int mem_id);// 로그인유저의 거래정보
 }
