acbe09177397873eb2c01c15eef3565c3e152381
HOMEPC-CHOI <HOMEPC-CHOI@DESKTOP-11C0S11>
2017-11-16 22:30:50 +0900

    SoldDao수정

--- BookMarket_01/src/dao/SoldDao.java
+++ BookMarket_01/src/dao/SoldDao.java
@@ -148,7 +148,7 @@ public class SoldDao implements ISoldDao{
 		return list;
 	}
 	public List<Sold> selectSoldListByLoginUser(int mem_id){
-		String sql = "select * from (select * from sold where seller_id = ? UNION select * from sold where buyer_id = ?) j order by sold_date desc";
+		String sql = "select * from (select * from sold where seller_id = ? UNION select * from sold where buyer_id = ?) j order by sold_date desc limit 5";
 		List<Sold> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
